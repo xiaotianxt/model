03d80a40cd390db9a676eb74c63a7551cade40da
xiaotianxt <tianyp@pku.edu.cn>
2023-04-10 14:09:01 +0800

    feat: introduce zustand for global store

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,7 +1,11 @@
 export default function Playground() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      Model Generation
+      <div>Control panel</div>
+      <div className="flex flex-1 flex-col min-h-full min-w-full justify-center items-center place-items-center">
+        Main Canvas
+      </div>
+      <div>Event log</div>
     </main>
   );
 }
---
+++ app/store/model.ts
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+export interface ModelStore {
+  
+}
+
+export const modelStore = create<ModelStore>((set, get) => {
+  return {};
+});
